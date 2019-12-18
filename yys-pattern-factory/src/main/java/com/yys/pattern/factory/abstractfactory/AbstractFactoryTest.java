5a3db139066585c6abe5d01a267082a0e5ca1bfe
zhaojun <15313808562@163.com>
2019-12-18 18:05:30 +0800

    20191218-完善

--- yys-pattern-factory/src/main/java/com/yys/pattern/factory/abstractfactory/AbstractFactoryTest.java
+++ yys-pattern-factory/src/main/java/com/yys/pattern/factory/abstractfactory/AbstractFactoryTest.java
@@ -1,7 +1,17 @@
 package com.yys.pattern.factory.abstractfactory;
 
+/**
+ * 抽象工厂模式
+ *      Test
+ * @author yys
+ */
 public class AbstractFactoryTest {
 
+    /*
+     * 缺点：
+     *      1、规定了所有可能被创建的产品集合，产品族中扩展新的产品困难，需要修改抽象工厂的接口
+     *      2、增加了系统的抽象性和理解难度
+     */
     public static void main(String[] args) {
 
         IMartialArtsFactory factory = new XingChenBianArtsFactory();
