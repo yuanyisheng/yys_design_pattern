fa2fcd13bbbf9ab6bfc147f521b0d767979f9224
zhaojun <15313808562@163.com>
2020-05-27 15:34:49 +0800

    strategy-20200527-完善

--- yys-pattern-strategy/src/main/java/com/yys/pattern/strategy/pay/StrategyTest.java
+++ yys-pattern-strategy/src/main/java/com/yys/pattern/strategy/pay/StrategyTest.java
@@ -9,6 +9,15 @@ import com.yys.pattern.strategy.pay.payport.PayStrategy;
  */
 public class StrategyTest {
 
+//    策略模式的优缺点
+//    优点：
+//          1、策略模式符合开闭原则。
+//          2、避免使用多重条件转移语句，如 if...else...语句、switch 语句
+//          3、使用策略模式可以提高算法的保密性和安全性。
+//    缺点： 
+//          1、客户端必须知道所有的策略，并且自行决定使用哪一个策略类。
+//          2、代码中会产生非常多策略类，增加维护难度
+
     public static void main(String[] args) {
 
         // 省略把商品添加到购物车，再从购物车下单
